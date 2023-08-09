90c3934986654376a42264b2c5d5a65384382a36
Amr M. Elabsy <36818188+AmrElabsy@users.noreply.github.com>
2023-08-10 02:54:46 +0300

    Update sidebar.blade.php

--- resources/views/components/common/sidebar.blade.php
+++ resources/views/components/common/sidebar.blade.php
@@ -74,7 +74,7 @@
 						 fill="none" stroke-linecap="round" stroke-linejoin="round">
 						<line x1="5" y1="12" x2="19" y2="12"></line>
 					</svg>
-					<span>Pages</span>
+					<span>The Pages</span>
 				</h2>
 
 				<li class="nav-item">
