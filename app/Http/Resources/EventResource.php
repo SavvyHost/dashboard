12b60f65eb46b95537884c27a23e0a8ca5d65215
shrouk.yahia.3 <shrouk.yahia.3@gmail.com>
2023-08-17 06:13:34 -0700

    optimize events

--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -14,17 +14,28 @@ class EventResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-//        return parent::toArray($request);
-		
-		return [
-			'id' => $this->id,
-			'title' => $this->title,
-			'image' => asset( $this->image ),
-			'avatar' => asset( $this->avatar ),
-			'content' => $this->content,
-			'start_date' => $this->start_date,
-			'end_date' => $this->end_date,
-			'location' => $this->location,
-		];
+        //        return parent::toArray($request);
+
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'image' => asset($this->image),
+            'avatar' => asset($this->avatar),
+            'content' => $this->content,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'location' => $this->location,
+            'searchable' => $this->searchable,
+            'seo_title' => $this->seo_title,
+            'seo_image' => asset($this->seo_image),
+            'seo_description' => $this->seo_description,
+            'facebook_title' => $this->facebook_title,
+            'facebook_image' => asset($this->facebook_image),
+            'facebook_description' => $this->facebook_description,
+            'twitter_title' => $this->twitter_title,
+            'twitter_image' => asset($this->twitter_image),
+            'twitter_description' => $this->twitter_description,
+
+        ];
     }
 }
