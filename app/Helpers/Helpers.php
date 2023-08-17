f4c977017962888f831ac83802e3bb40251dec76
shrouk.yahia.3 <shrouk.yahia.3@gmail.com>
2023-08-17 04:31:42 -0700

    d

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -3,26 +3,28 @@
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 
-function uploadImage( $image, $folder )
+function uploadImage($image, $folder)
 {
-	$path = createFolderIfNotExist($folder);
-	return getImagePath($image, $path);
+    $path = createFolderIfNotExist($folder);
+    return getImagePath($image, $path);
 }
 
-function getImagePath( $image, $path ) {
-	$image_name = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
-	$image->storeAs('public/' . $path, $image_name);
-	
-	return 'storage/' . $path . "/" . $image_name;
+function getImagePath($image, $path)
+{
+    $image_name = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+    // $image->storeAs('public/' . $path, $image_name);
+    return $image->storeAs('public/' . $path, $image_name);
+
+    // return 'storage/' . $path . "/" . $image_name;
 }
 
-function createFolderIfNotExist( $folder ): string
+function createFolderIfNotExist($folder): string
 {
-	$path = 'assets/' . $folder;
-	
-	if ( !Storage::exists('public/' . $path) ) {
-		Storage::disk('public')->makeDirectory($path);
-	}
-	
-	return $path;
+    $path = 'assets/' . $folder;
+
+    if (!Storage::exists('public/' . $path)) {
+        Storage::disk('public')->makeDirectory($path);
+    }
+
+    return $path;
 }
