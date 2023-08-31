bf0f3c19ab32a2cfc84d3f11b62db0279506bd94
shrouk.yahia.3 <shrouk.yahia.3@gmail.com>
2023-08-30 17:18:16 -0700

    Make Social Auth

--- app/Http/Controllers/OAuth/SocialAuthController.php
+++ app/Http/Controllers/OAuth/SocialAuthController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\OAuth;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
+use SebastianBergmann\CliParser\Exception;
 
 class SocialAuthController extends Controller
 {
